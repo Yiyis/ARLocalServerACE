51a758c962f54c524c1f78492b2b85c39d33693c
Yiyi <414956851@qq.com>
2018-11-30 18:33:56 -0500

    First commit, servo toggle finished

---
+++ Assets/Scripts/button.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class button : MonoBehaviour {
+    private const string URL = "http://192.168.4.1/servo";
+	public Text responseText;
+
+	public void Request()
+	{ 
+		StartCoroutine (Upload());
+	
+	}
+
+
+	private IEnumerator Upload()
+	{
+		//WWWForm form = new WWWForm();
+        //form.AddField("/servo", "toggleServo");
+
+		UnityWebRequest www = UnityWebRequest.Post((URL), "toggleServo");
+		www.chunkedTransfer = false;////ADD THIS LINE
+		yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+
+	}
+}
